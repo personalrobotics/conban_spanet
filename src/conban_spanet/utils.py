fdcaa9ed8bf18fd1105e851cea2fb5540adec3e0
mengeks <mengeks@users.noreply.github.com>
2019-05-28 18:42:02 -0700

    Create utils.py

---
+++ src/conban_spanet/utils.py
@@ -0,0 +1,12 @@
+def array_map(f, x):
+    return np.array(list(map(f, x)))
+
+def oracle(CBAlgo, feature_n_t, a_t, c_t, p_a_t):
+    "CBAlgo: ContextualBanditAlgo object"
+    A_a_t, b_a_t = CBAlgo.A[a_t], CBAlgo.b[a_t]
+    A_a_t += np.outer(feature_n_t, feature_n_t) / p_a_t
+    b_a_t += c_t / p_a_t * feature_n_t
+    theta_a_t = np.linalg.solve(A_a_t, b_a_t)
+    CBAlgo.A[a_t] = A_a_t
+    CBAlgo.b[a_t] = b_a_t
+    CBAlgo.theta[a_t] = theta_a_t
