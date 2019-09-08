3ddaa835501d4f99aa9ee73a94b1b4e1d855f7c3
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2019-09-08 11:11:25 -0700

    Allow user to specify algorithm

--- scripts/run_ros_server.py
+++ scripts/run_ros_server.py
@@ -11,8 +11,10 @@ TODO:
   * Use arguments to define `algo`
 """
 
+import argparse
+import os
 import rospy
-from conban_spanet.conbanalg import singleUCB
+from conban_spanet.conbanalg import ContextualBanditAlgo, epsilonGreedy, singleUCB, multiUCB, MultiArmedUCB
 
 from food_detector.srv import GetAction, PublishLoss, GetActionResponse, PublishLossResponse
 
@@ -59,9 +61,54 @@ def create_server(algo, server_name=SERVER_NAME):
     start_publish_loss(algo)
 
 if __name__ == '__main__':
-    # TODO: add options for other CONBANs
-    algo = singleUCB(N=1, alpha=0.05, gamma=0)
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('-a', '--algo', default="greedy",
+		    type=str, help='algorithm to use: greedy, epsilon, singleUCB, multiUCB, UCB')
+    ap.add_argument('-alp', '--alpha', default=0.05,
+                    type=float, help='alpha for LinUCB')
+    # TODO: Is this a good default for delta?
+    ap.add_argument('-d', '--delta',default=0,
+                    type=float, help="delta for MultiArmedUCB")
+    ap.add_argument('-e', '--epsilon', default=0,
+                    type=float, help='epsilon for epsilon greedy')
+    ap.add_argument('-ga', '--gamma',default=1000,
+                    type=float, help='gamma for singleUCB')
+    ap.add_argument('-g', '--gpu', default='0', type=str, help='GPU ID')
+
+    args = ap.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    N=1
+
+    # Initialize ContextualBanditAlgo
+    if  args.algo == 'greedy':
+        print('Standard contextual bandit chosen')
+        algo = ContextualBanditAlgo(N=N)
+
+    elif args.algo == 'epsilon':
+        print('Epsilon-greedy contextual bandit chosen with epsilon {}'.format(args.epsilon))
+	algo = epsilonGreedy(N=N, epsilon=args.epsilon)
+
+    elif args.algo == 'singleUCB':
+        print('Single UCB chosen with alpha {} and gamma {}'.format(args.alpha, args.gamma))
+	algo = singleUCB(N=N, alpha=args.alpha, gamma=args.gamma)
+
+    elif args.algo == 'multiUCB':
+        print('Multi UCB chosen')
+	algo = multiUCB(N=N)
+
+    elif args.algo == 'UCB':
+        print('Multiarmed UCB chosen with delta {}'.format(args.delta))
+        # TODO: should we set the horizon, T?
+	algo = MultiArmedUCB(N=N, delta=args.delta)
+
+    else:
+        exit('"{}" is not a valid algorithm type'.format(args.algo))
+
     create_server(algo)
+
     try:
         print('Running {} server'.format(SERVER_NAME))
         rospy.spin()
