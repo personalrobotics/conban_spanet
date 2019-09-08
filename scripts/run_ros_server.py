5bd538d329617996e0383f7619b37621a2a60605
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2019-09-08 10:38:52 -0700

    Unflatten p_t for learning and refactor server and service launching

--- scripts/run_ros_server.py
+++ scripts/run_ros_server.py
@@ -18,34 +18,53 @@ from food_detector.srv import GetAction, PublishLoss, GetActionResponse, Publish
 
 SERVER_NAME = 'conban_spanet_server'
 
-# TODO: add options for other CONBANs
-algo = singleUCB(N=1, alpha=0.05, gamma=0)
-
-def handle_get_action(req):
+def _handle_get_action(req, algo):
     p_t = algo.explore(req.features)
 
     # Sample Action
     _, K = p_t.shape
     p_t_flat = p_t.reshape((-1,))
-    sample_idx = np.random.choice(N*K, p = p_t_flat)
+    sample_idx = np.random.choice(K, p=p_t_flat)
     a_t = sample_idx % K
 
-    return srv.GetActionResponse(a_t, p_t)
+    return srv.GetActionResponse(a_t, p_t_flat)
 
-def handle_publish_loss(req):
+def _handle_publish_loss(req, algo):
     try:
-        algo.learn(req.features, 0, req.a_t, req.loss, req.p_t)
+        p_t = np.expand_dims(req.p_t, axis=0)
+        algo.learn(req.features, 0, req.a_t, req.loss, p_t)
     except:
         return False
     return True
 
-if __name__ == '__main__':
+def start_get_action(algo):
+    """Starts the `GetAction` service with a given algorithm"""
+    def handle_wrapper(req):
+        _handle_get_action(req, algo)
+    rospy.Service('GetAction', GetAction, handle_wrapper)
+
+def start_publish_loss(algo):
+    """Starts the `PublishLoss` service with a given algorithm"""
+    def handle_wrapper(req):
+        _handle_publish_loss(req, algo)
+    rospy.Service('PublishLoss', PublishLoss, handle_wrapper)
+
+def create_server(algo, server_name=SERVER_NAME):
+    """
+    Creates the algorithm server with a given algorithm.
+    Provides the services `GetAction` and `PublishLoss`.
+    """
     rospy.init_node(SERVER_NAME)
-    rospy.Service('GetAction', GetAction, handle_get_action)
-    rospy.Service('PublishLoss', PublishLoss, handle_publish_loss)
+    start_get_action(algo)
+    start_publish_loss(algo)
+
+if __name__ == '__main__':
+    # TODO: add options for other CONBANs
+    algo = singleUCB(N=1, alpha=0.05, gamma=0)
+    create_server(algo)
     try:
-        print('Server running')
+        print('Running {} server'.format(SERVER_NAME))
         rospy.spin()
     except KeyboardInterrupt:
         pass
-    print('Shutting down {}...'.format(SERVER_NAME))
+    print('\nShutting down {}...'.format(SERVER_NAME))
