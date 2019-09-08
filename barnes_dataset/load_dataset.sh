9e2d1c0458d7112cee702688b20b0fd1791951dd
Xiang Meng <garethalexuni@gmail.com>
2019-09-08 16:59:35 -0700

    Updated sh file for barnes dataset

--- barnes_dataset/load_dataset.sh
+++ barnes_dataset/load_dataset.sh
@@ -7,7 +7,7 @@
 # scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/bite_selection_package/checkpoint/spnet_ckpt.pth ./checkpoint/
 
 echo "load food dataset from mthrbrn"
-scp -r conban@mthrbrn.cs.washington.edu:/home/conban/conban_ws/src/conban_spanet/barnes_dataset/* .
+scp -r conban@mthrbrn.personalrobotics.cs.washington.edu:/home/conban/conban_ws/src/conban_spanet/barnes_dataset/* .
 
 # echo "load label_map definition from mthrbrn"
 # if [ ! -d "./data" ]; then
